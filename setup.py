fa7bc05adbc3fa9cd004113384f164bcc995811e
István Bozsó <bozso.istvan@csfk.mta.hu>
2020-04-03 15:21:50 +0200

    generation and zipping of html notes implemented; installation tutorial for anaconda added

---
+++ setup.py
@@ -0,0 +1,36 @@
+from nbconvert.exporters.html import HTMLExporter
+import shutil as sh
+import os
+import zipfile as zf
+import nbformat
+
+import utils
+
+exporter = HTMLExporter()
+
+Root = utils.Path(__file__).abspath().dirname()
+html = Root.join("html")
+outdir = html.join("notebooks")
+
+def main():
+    if 0:
+        for notebook in Root.join("notebooks_hu", "*.ipynb").iglob():
+            contents = notebook.read_all()
+            contents = nbformat.reads(contents, as_version=4)
+    
+            body, _ = exporter.from_notebook_node(contents)
+            
+            out = notebook.basename().replace_ext("html")
+            
+            with outdir.join(out).open("w") as f:
+                f.write("".join(body))
+    
+    if 1:
+        with utils.cd(html):
+            sh.make_archive(
+                Root.join("releases", "jegyzet").get_path(),
+                "zip",
+            )    
+    
+if __name__ == "__main__":
+    main()
